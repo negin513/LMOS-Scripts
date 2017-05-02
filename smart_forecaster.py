0deae036a667916eff74d89327675b5082644673
Maryam Abdioskouei <mabdioskouei@argon-login-1.hpc>
2017-05-02 16:58:35 -0500

    few changes

--- smart_forecaster.py
+++ smart_forecaster.py
@@ -14,6 +14,7 @@ from namelist_maker import make_wrf_namelist
 from qsub_maker import make_qsub
 from job_submitter import submit_job
 from macc_download_maker import make_macc_download
+
 ## What do we do?
 ## 1) 
     
@@ -34,55 +35,57 @@ def get_parser():
     	parser = ArgumentParser(description=__doc__,
                             formatter_class=ArgumentDefaultsHelpFormatter)
     	parser = argparse.ArgumentParser(description='Setting the flags and input file.')
-    	#parser.add_argument('-T', '--tag', help='Add a tag to your work.',required=True, action="store", dest="tag")
     	parser.add_argument('-sst', '--sst', help='Get SST analyses grib version 1 format!'  , action="store_true", dest="sst_switch", default=False)
-    	parser.add_argument('--now', help='Get SST analyses grib version 1 format!'  , action="store_true", dest="now_switch", default=False)
-    	parser.add_argument('-fir', '--fire', help='Get SST analyses grib version 1 format!'  , action="store_true", dest="sst_switch", default=False)
+    	parser.add_argument('--forecast','--now', help='Get SST analyses grib version 1 format!' , action="store_true", dest="now_switch", default=True)
     	return parser	
 
 if __name__ == "__main__":
 	
-	## 1) Set your directories
+    ## 1) Set your directories
     script_dir   = os.path.dirname(os.path.abspath(__file__))
     parent_dir   = os.path.dirname(script_dir)
     result_dir   = parent_dir+ "/results/"
-    wrf_dir   = parent_dir+ "/WRFV3/test/em_real/"
-    wps_dir   = parent_dir+ "/WPS/"
-    macc_dir  = parent_dir+"/macc_data/"
+    wrf_dir      = parent_dir+ "/WRFV3/test/em_real/"
+    wps_dir      = parent_dir+ "/WPS/"
+    gfs_dir      = parent_dir+ "/gfs_data/"
+    macc_dir     = parent_dir+ "/macc_data/"
+
+
+    sim_length_hour      = 48 # simulation length hours  
+    download_length_hour = 72 # Ask Maryam?!
+   
     args         = get_parser().parse_args()
     if args.sst_switch:
-        print "\nWTF\n"
-
-    sim_length_hour = 48 # simulation length hours
-    download_length_hour = 72 
-   
+        print "\nSST Does not work for now!!!\n"
+    
     if args.now_switch:
         sim_start_time = datetime.now()
         print sim_start_time
-    else:
-        sim_start_time    = datetime(2017,04,01,00,00,00,00) ### CAN ADD TZ INFO 
-        print sim_start_time
+    #else:  
+    #    sim_start_time    = datetime(2017,04,01,00,00,00,00) ### CAN ADD TZ INFO 
+    #    print sim_start_time
         
     sim_end_time    = sim_start_time + timedelta(hours= sim_length_hour )
     print sim_end_time
 
     sim_start_date = sim_start_time.strftime('%Y%m%d')
     print sim_start_date
-	## 2) step 2: GFS DATA
-    gfs_run = '06' ## WHAT IS THIS?
+    
+    ## 2) step 2: GFS DATA
+    gfs_run = '06'
 
     ch_dir(parent_dir)
-    mk_dir('gfs_data')
-    ch_dir('gfs_data')
+    mk_dir(gfs_dir)
+    ch_dir(gfs_dir)
     mk_dir(sim_start_date)
     ch_dir(sim_start_date)
 
     # GFS data for today is available
-    if (args.now_switch or sim_start_time.date()==datetime.now()):
-        for i in range(0,download_length_hour+3,3):
-		    #downloading data
-		    print "not downloading"
-		    #os.system('wget http://tgftp.nws.noaa.gov/SL.us008001/ST.opnl/MT.gfs_CY.'+gfs_run+'/RD.'+sim_start_date+'/PT.grid_DF.gr2/fh.'+str(i).zfill(4) +'_tl.press_gr.0p50deg')
+    #if (args.now_switch or sim_start_time.date()==datetime.now()):
+    for i in range(0,download_length_hour+3,3):
+	#downloading data
+	print "not downloading"
+	#os.system('wget http://tgftp.nws.noaa.gov/SL.us008001/ST.opnl/MT.gfs_CY.'+gfs_run+'/RD.'+sim_start_date+'/PT.grid_DF.gr2/fh.'+str(i).zfill(4) +'_tl.press_gr.0p50deg')
     
     # MACC date
     #mac_start_time    = sim_start_time - timedelta(hours= 48)
